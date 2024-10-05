684e3ab30d1eb7190a2eb90eefd312d9d2a0dccd
황윤진 <dbswlsghkd@gmail.com>
2024-10-05 19:34:43 +0900

    MVC 역할과 실행 흐름

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -9,7 +9,15 @@ public class FirstController {
 
     @GetMapping("/hi")
     public String niceToMeetYou(Model model){
+        // 모델
         model.addAttribute("username", "윤진");
         return "greetings"; //templates/greetings.mustache -> 브라우저로 전송!
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        // 모델
+        model.addAttribute("nickname", "홍길동");
+        return "goodbye"; //templates/greetings.mustache -> 브라우저로 전송!
     }
 }

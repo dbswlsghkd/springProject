c8688cfa548d5d3b5e053190490c0b18812c6965
황윤진 <dbswlsghkd@gmail.com>
2024-10-06 19:36:30 +0900

    데이터 목록 조회

--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,8 +3,11 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 // spring에서 제공해주는 CrudRepository 기능 사용가능
 // CRUD 기능 사용가능
 public interface ArticleRepository extends CrudRepository<Article, Long> {
-
+    @Override
+    ArrayList<Article> findAll();
 }

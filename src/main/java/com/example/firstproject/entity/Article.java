b32a899f530c6a9d38c2f922bb90efff1599d9cd
황윤진 <dbswlsghkd@gmail.com>
2024-10-07 21:42:46 +0900

    data.sql 추가, id 자동 채번

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +13,8 @@ import lombok.ToString;
 @Getter
 public class Article {
     @Id // 대표값을 지정 like a 주민등록번호
-    @GeneratedValue // 1, 2, 3 자동 생성 어노테이션
+//    @GeneratedValue // 1, 2, 3 자동 생성 어노테이션
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성 전략
     private Long id;
 
     @Column // db 테이블과 연동

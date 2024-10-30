64e11e8a23e84578e3a70efb9bdab91c3247dd5e
황윤진 <dbswlsghkd@gmail.com>
2024-10-30 20:28:56 +0900

    front-end mustache 대신 react 연동 작업

--- src/main/java/com/example/firstproject/config/SecurityConfig.java
+++ src/main/java/com/example/firstproject/config/SecurityConfig.java
@@ -76,20 +76,20 @@ public class SecurityConfig {
         http
                 .httpBasic((auth) -> auth.disable());
 
-        http    //특정한 경로에 허용, 거부 가능(람다식으로 작성해야함)
-                .authorizeRequests((auth) -> auth
-                    // 정적 리소스 허용 (css, js 등)
-                .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
-                // root, login 페이지에서 특정한 작업을 실행하려고 할 때
-                // 모든 사용자가 로그인 하지 않아도 접근 가능하도록 permitAll
-                .requestMatchers("/", "/login", "/register", "/articles/**", "/api/register").permitAll()
-                // .requestMatchers("/articles").authenticated()  // 인증된 사용자만 접근 가능
-                .requestMatchers("/parts").hasRole("ADMIN")
-                // 와일드 카드를 통해 여러 유저 접근
-                // .requestMatchers("/my/**").hasAnyRole("USER", "ADMIN")
-                // 그 외 막지 못하는 경우 로그인을 통해 접근할 수 있도록 설정
-                .anyRequest().authenticated()
-            );
+        // http    //특정한 경로에 허용, 거부 가능(람다식으로 작성해야함)
+        //         .authorizeRequests((auth) -> auth
+        //             // 정적 리소스 허용 (css, js 등)
+        //         .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+        //         // root, login 페이지에서 특정한 작업을 실행하려고 할 때
+        //         // 모든 사용자가 로그인 하지 않아도 접근 가능하도록 permitAll
+        //         .requestMatchers("/", "/login", "/register", "/articles/**", "/api/register").permitAll()
+        //         // .requestMatchers("/articles").authenticated()  // 인증된 사용자만 접근 가능
+        //         .requestMatchers("/parts").hasRole("ADMIN")
+        //         // 와일드 카드를 통해 여러 유저 접근
+        //         // .requestMatchers("/my/**").hasAnyRole("USER", "ADMIN")
+        //         // 그 외 막지 못하는 경우 로그인을 통해 접근할 수 있도록 설정
+        //         .anyRequest().authenticated()
+        //     );
 
         http
                 .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);

75b109b734666dd5639b9bfbd7d70e9072cd2e46
황윤진 <dbswlsghkd@gmail.com>
2024-11-27 22:08:53 +0900

    금형관리 JPA -> mybatis로 변경완료

---
+++ src/main/java/com/example/firstproject/repository/MoldMapper.java
@@ -0,0 +1,58 @@
+package com.example.firstproject.repository;
+
+import com.example.firstproject.entity.Mold;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+@Slf4j
+@RequiredArgsConstructor
+public class MoldMapper {
+
+    private final SqlSessionTemplate sqlSessionTemplate;
+
+    public Page<Mold> findBySearch(String searchTerm, Pageable pageable) {
+        // log.info("안에 들어와요?");
+        // 페이징 파라미터 생성
+        Map<String, Object> params = new HashMap<>();
+        params.put("searchTerm", "%" + searchTerm + "%");
+        params.put("offset", pageable.getPageNumber() * pageable.getPageSize());
+        params.put("pageSize", pageable.getPageSize());
+
+        log.info("findBySearch 쿼리 실행 전에 도달했습니다");
+        // 데이터 조회
+        List<Mold> molds = sqlSessionTemplate.selectList("com.example.firstproject.mapper.MoldMapper.findBySearch", params);
+        log.info("findBySearch 쿼리 실행 후 결과: {}", molds);
+        // 전체 개수 조회
+        int total = sqlSessionTemplate.selectOne("com.example.firstproject.mapper.MoldMapper.countMoldsSearch", params);
+
+        // Page 객체로 변환
+        return new PageImpl<>(molds, pageable, total);
+    }
+
+
+    public Page<Mold> findMoldAll(Pageable pageable) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("offset", pageable.getPageNumber() * pageable.getPageSize());
+        params.put("pageSize", pageable.getPageSize());
+
+        log.info("findPartBy 쿼리 실행 전에 도달했습니다");
+        // 데이터 조회
+        List<Mold> molds = sqlSessionTemplate.selectList("com.example.firstproject.mapper.MoldMapper.findMoldAll", params);
+        log.info("findPartBy 쿼리 실행 후 결과: {}", molds);
+        // 전체 개수 조회
+        int total = sqlSessionTemplate.selectOne("com.example.firstproject.mapper.MoldMapper.countMolds", params);
+
+        // Page 객체로 변환
+        return new PageImpl<>(molds, pageable, total);
+    }
+}
--- src/main/java/com/example/firstproject/service/MoldService.java
+++ src/main/java/com/example/firstproject/service/MoldService.java
@@ -1,7 +1,10 @@
 package com.example.firstproject.service;
 
 import com.example.firstproject.entity.Mold;
+import com.example.firstproject.entity.Part;
+import com.example.firstproject.repository.MoldMapper;
 import com.example.firstproject.repository.MoldRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,18 +13,25 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class MoldService {
 
     @Autowired
     private MoldRepository moldRepository;
 
+    private final MoldMapper moldMapper;
+
     public Page<Mold> index(Pageable pageable) {
-        return moldRepository.findMoldAll(pageable);
+        // return partRepository.findPartBy(pageable);
+        log.info("index==========> 여기 들어와" );
+        return moldMapper.findMoldAll(pageable);
     }
 
     public Page<Mold> searchMold(String searchTerm, Pageable pageable) {
         String searchPattern = "%" + searchTerm + "%";
-        return moldRepository.findMoldByPcode(searchPattern, pageable);
+        // return partRepository.findBySearch(searchPattern, pageable);
+        log.info("searchParts ==========> 여기 들어와" );
+        return moldMapper.findBySearch(searchPattern, pageable);
     }
 
 }
